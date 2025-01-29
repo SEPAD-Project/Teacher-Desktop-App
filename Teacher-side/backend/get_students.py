9f017317c7b1b3819f9e356f4eb112221faa6e77
Abolfazl Rashidian <abolf4zlrashidian@gmail.com>
2025-01-29 16:46:25 +0330

    getting students list and their message

--- Teacher-side/backend/get_students.py
+++ Teacher-side/backend/get_students.py
@@ -1,43 +1,42 @@
 import requests
 import time
 
-# آدرس سرور
+# server address
 server_url = "http://localhost:5001"
 
-# school and class
-school_name = input("Enter school name: ").strip()
-class_code = input("Enter class code: ").strip()
 
 # getting students list
-response = requests.post(f"{server_url}/get_students", json={"school_name": school_name, "class_code": class_code})
+def get_students_list(school_name, class_code):
+    try:
+        response = requests.post(f"{server_url}/get_students", json={"school_name": school_name, "class_code": class_code})
 
-if response.status_code != 200:
-    print("Error while fetching students:", response.json().get("error", "Unknown error"))
-    exit()
+        if response.status_code != 200:
+            return [False, 'no school or class found'] # , response.json().get("error", "Unknown error")
+        else:
+            students = response.json().get("students", [])
+            if not students:
+                return [False, 'No Students found']
+            else:
+                return [True, students]
+    except Exception:
+        return [False, 'Error']
 
-students = response.json().get("students", [])
-print("Students in class:", students)
+# print(get_students_list('hn1', '1052'))
 
-if not students:
-    print("No students found in this class.")
-    exit()
 
 # getting students message and show them
-def fetch_messages():
-    while True:
-        for student in students:
-            response = requests.post(f"{server_url}/get_last_message", json={
-                "school_name": school_name,
-                "class_code": class_code,
-                "student_name": student
-            })
-            if response.status_code == 200:
-                data = response.json()
-                print(f"{data['student']}: {data['message']}")
-            else:
-                print(f"Error fetching message for {student}")
+def fetch_messages(student, school_name, class_code):
+    response = requests.post(f"{server_url}/get_last_message", json={
+            "school_name": school_name,
+            "class_code": class_code,
+            "student_name": student
+    })
+    if response.status_code == 200:
+        data = response.json()
+        return [True, data['message']]
+    else:
+        return [False, 'Error']
         
-        print("\n--- Waiting for next update ---\n")
-        time.sleep(30)
+fetch_messages('Reza', 'hn1', '1052')
 
-fetch_messages()
+# fetch_messages()
