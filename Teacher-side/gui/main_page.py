5e5496abb54c6c352b312d5612b7880d1c659c90
Abolfazl Rashidian <abolf4zlrashidian@gmail.com>
2025-01-29 16:47:05 +0330

    connected to backend for getting students and their status

--- Teacher-side/gui/main_page.py
+++ Teacher-side/gui/main_page.py
@@ -1,10 +1,20 @@
 from customtkinter import CTk, CTkButton, CTkFrame, CTkLabel
 from tkinter import ttk, CENTER, BOTH, RIGHT, Y, VERTICAL
+from pathlib import Path
+import sys
+from threading import Thread
 
+parent_dir = Path(__file__).resolve().parent.parent
+sys.path.append(str(parent_dir / "backend"))
+
+from get_students import get_students_list, fetch_messages
 
 class MainPage(CTk):
-    def __init__(self):
+    def __init__(self, class_id):
         super().__init__()
+
+        self.class_id = class_id
+        self.student_rows = {}
 
         self.title("Main Page")
         self.geometry('900x600')
@@ -21,23 +31,28 @@ class MainPage(CTk):
         # Creating Treeview (Table)
         self.table = ttk.Treeview(
             master=self.element_frame,
-            columns=("Name", "face1", "face2", "face3", "Accuracy rate", "Desktop"),
+            columns=("Name", "Last Check & Time", "Accuracy rate", "Desktop"),
             show='headings',
             height=15
         )
-
+        self.students_list = get_students_list(school_name=str(self.class_id).split('-')[0], class_code=str(self.class_id).split('-')[1])
+        if self.students_list[0] :
+            self.students_list = self.students_list[1]
+            for student in self.students_list:
+                item_id = self.table.insert("", "end", values=(student, 'N/A', 'N/A', 'N/A'))
+                self.student_rows[student] = item_id  # ذخیره `item_id`
         # Defining Columns
-        for col in ("Name", "face1", "face2", "face3", "Accuracy rate", "Desktop"):
+        for col in ("Name", "Last Check & Time", "Accuracy rate", "Desktop"):
             self.table.heading(col, text=col, anchor=CENTER)
-            if col == 'Name' or col == 'Accuracy rate' or col == 'Desktop' :
-                self.table.column(col, anchor=CENTER, width=150)
+            if col == 'Name' or col == 'Last Check & Time' :
+                self.table.column(col, anchor=CENTER, width=180)
             else:
-                self.table.column(col, anchor=CENTER, width=50)
+                self.table.column(col, anchor=CENTER, width=100)
 
 
         # Adding Sample Data
-        for i in range(1, 31):
-            self.table.insert("", "end", values=(f"Row {i} Col 1", f"Row {i} Col 2", f"Row {i} Col 3", f"Row {i} Col 4", f"Row {i} Col 5", f"Row {i} Col 6", f"Row {i} Col 7"))
+        # for i in range(1, 31):
+        #     self.table.insert("", "end", values=(f"Row {i} Col 1", f"Row {i} Col 2", f"Row {i} Col 3", f"Row {i} Col 4", f"Row {i} Col 5", f"Row {i} Col 6", f"Row {i} Col 7"))
 
         # Adding Scrollbar
         self.scrollbar = ttk.Scrollbar(self.element_frame, orient=VERTICAL, command=self.table.yview)
@@ -48,21 +63,34 @@ class MainPage(CTk):
         self.scrollbar.grid(row=1, column=0, sticky='nes', pady=20)
 
         # elements 
-        self.main_label = CTkLabel(master=self.element_frame, text='Students Status', font=('montserrat', 30, 'bold'))
+        self.main_label = CTkLabel(master=self.element_frame, text=f'Students Class {self.class_id} Status', font=('montserrat', 30, 'bold'))
         self.exit_button = CTkButton(master=self.element_frame, text='Exit', font=('montserrat', 20, 'bold'), height=30, width=250, fg_color='red', hover_color='#6B0011', border_color='white', border_width=2, command=lambda: self.destroy())
 
         # placing elements in element_frame      
         self.main_label.grid(row=0, column=0 ,sticky='n')
         self.exit_button.grid(row=2, column=0, pady=(10,0), sticky='we')
 
+        Thread(target=self.update_data, daemon=True).start()
+
+
+    def update_data(self):
+        for student in self.students_list :
+            respond = fetch_messages(student=student, school_name=str(self.class_id).split('-')[0], class_code=str(self.class_id).split('-')[1])
+            if respond[0] :
+                self.table.item(self.student_rows[student], values=(student, respond[1], "N/A", "N/A"))
+        self.after(30000, self.update_data)
+
+    def get_last_status(self):
+        self.students_list
+
     def run(self):
         self.mainloop()
 
 
-def main_page_func():
-    app = MainPage()
+def main_page_func(classid):
+    app = MainPage(classid)
     app.run()
 
 
 if __name__ == '__main__' : 
-    main_page_func()
+    main_page_func('hn1-1052')
