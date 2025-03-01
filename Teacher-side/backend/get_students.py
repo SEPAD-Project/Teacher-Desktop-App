26ff647e67a62e832ddb2c7c20b89ccf040ba015
Abolfazl Rashidian <abolf4zlrashidian@gmail.com>
2025-03-01 16:03:11 +0330

    fetch_messages function removed from tihs file

--- Teacher-side/backend/get_students.py
+++ Teacher-side/backend/get_students.py
@@ -29,17 +29,3 @@ def get_students_list(school_name, class_code):
     except Exception as e:
         return [False, e]
 
-# getting students message and show them
-def fetch_messages(student, school_name, class_code):
-    response = requests.post(f"{server_url}/get_last_message", json={
-            "school_name": school_name,
-            "class_code": class_code,
-            "student_name": student
-    }, timeout=3)
-    if response.status_code == 200:
-        data = response.json()
-        return [True, data['message']]
-    else:
-        return [False, 'Error']
-        
-
